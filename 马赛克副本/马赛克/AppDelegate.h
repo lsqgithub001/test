19a2412925c95156d135c568372e573aa69c1210
superLai <225546147@qq.com>
2018-01-22 23:11:09 +0800

    masyk1
    version1

---
+++ 马赛克副本/马赛克/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  马赛克
+//
+//  Created by 刘桂林 on 16/6/27.
+//  Copyright (c) 2016年 刘桂林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
